e4f2cd7dd00934683f8c5745287ccad769e8bc17
tomcast <castthom@gmail.com>
2015-10-08 14:40:50 -0400

    Add Data handling and foundation ui
    Add foundation ui framework, remove unnecessary files.

---
+++ projects/mapTool/js/dataAdd.js
@@ -0,0 +1,10 @@
+var addDataButton = L.control({position: 'bottomright'});
+
+addDataButton.onAdd = function (map) {
+
+	var div = L.DomUtil.create('a', 'button radius alert tiny addData')
+	div.innerHTML = 'Add Data';
+	return div;
+};
+
+addDataButton.addTo(map);
