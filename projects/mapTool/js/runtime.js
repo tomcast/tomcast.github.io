ef98285e8d2694a846360e094b7a47502d33a502
tomcast <castthom@gmail.com>
2015-10-08 15:30:46 -0400

    remove documentation
    remove documentation because bossman doesn't like the word "leaflet"
    apparently

--- projects/mapTool/js/runtime.js
+++ projects/mapTool/js/runtime.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+	// connect modal button with the modal itself
     var modalHandle = document.getElementsByClassName('addData')[0]
 	modalHandle.setAttribute('data-reveal-id','AddDataModal');
+
+	// remove documentation for secret mode
+	document.getElementsByClassName('leaflet-control-attribution')[0].remove();
 });
