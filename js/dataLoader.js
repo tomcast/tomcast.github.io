2d9e6afd3e9a20c3b7a8b09fbfbc9fac2ee6070d
tomcast <castthom@gmail.com>
2015-11-11 09:46:49 -0500

    Retool homepage

--- js/dataLoader.js
+++
@@ -1,5 +0,0 @@
-var MichPoints = new L.GeoJSON.AJAX("data/michigan.json", 
-	{onEachFeature: function (feature, layer) {
-		layer.bindPopup(feature.properties.Station + " - " + feature.properties.Temp + "F");
-	}
-});
