6e0cfc34c13f4e2ce5b7f763cf8096c7e64f021e
tom <castthom@gmail.com>
2021-11-23 13:32:18 -0500

    Update Web work

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-//This script is for testing on a local machine only. Do not go live with
-//this script as your server. You will have issues and I will laugh at you.
+//This script is for testing on a local machine only.
 
 var express = require('express');
 var app = express();
