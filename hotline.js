066f2a27391af56aef266b3761d7ac88d4979125
Thomas Cast <castthom@gmail.com>
2022-11-25 22:40:38 -0500

    Prepare for rerun

--- hotline.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-
-  // If you want to keep full screen on window resize
-  $(window).resize(function(){
-    $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
-  });
-});
