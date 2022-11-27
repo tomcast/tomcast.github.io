50623e15b72145d02eee9de0cf5386bda80ae519
Thomas Cast <castthom@gmail.com>
2022-11-26 19:42:23 -0500

    Updates to custconv

---
+++ projects/CustConv/js/map_creation.js
@@ -0,0 +1,9 @@
+// File to handle the background map and toggle background map layers on and off.
+// Does not work with other data outside of background map.
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
