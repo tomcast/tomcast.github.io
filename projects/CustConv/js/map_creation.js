104b07e2ac289b7895b1a342aaf6eaac5133be9b
Thomas Cast <castthom@gmail.com>
2022-11-27 16:52:35 -0500

    More Updates to personal website

--- projects/CustConv/js/map_creation.js
+++ projects/CustConv/js/map_creation.js
@@ -1,22 +1,45 @@
 // File to handle the background map and toggle background map layers on and off.
 // Does not work with other data outside of background map.
 
-var map = L.map('map').setView([42.46, -83.18], 9);
 
+//handling functions
+function getColor(d) {
+	return d > 5555 ? '#b30000' :
+		d > 3969  ? '#e34a33' :
+		d > 2647  ? '#fc8d59' :
+		d > 1174  ? '#fdcc8a' :
+				   '#fef0d9' ;
+}
+
+
+//Create base map in basicMap div
+var basicMap = L.map('basicMap').setView([42.46, -83.18], 9);;
+
+//Load basemap tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+}).addTo(basicMap);
 
-var existing = new L.GeoJSON.AJAX("data/Locations.geojson", {
+
+//Load location data with popup
+var locations = new L.GeoJSON.AJAX("data/Locations.geojson", {
 	onEachFeature: function(feature, layer) {
 		layer.bindPopup(feature.properties.City + "</br>" + feature.properties.Address + ", " +
-		feature.properties.state + " " + feature.properties.ZIP);
+		feature.properties.State + " " + feature.properties.ZIP);
 	}
-}).addTo(map);
+}).addTo(basicMap);
 
+
+//Load and display the base geographic areas
 var areas = new L.GeoJSON.AJAX("data/areas.geojson", {
+	style: function (feature,layer) { return {
+		fillColor: getColor(feature.properties.pop_TOTAL),
+		fillOpacity: .7,
+		weight: 1,
+		color: 'white'}
+	},
 	onEachFeature: function(feature, layer) {
 		layer.bindPopup("Tract: " + feature.properties.GEOID + "</br> Population: " + feature.properties.pop_TOTAL);
 	}
-}).addTo(map);
+}).addTo(basicMap);
