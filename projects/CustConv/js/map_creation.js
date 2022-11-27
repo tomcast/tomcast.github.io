85505cd2601c4973a8c07b0ee3b764f8bd7b6568
Thomas Cast <castthom@gmail.com>
2022-11-27 15:10:23 -0500

    Update Raw Data

--- projects/CustConv/js/map_creation.js
+++ projects/CustConv/js/map_creation.js
@@ -1,7 +1,7 @@
 // File to handle the background map and toggle background map layers on and off.
 // Does not work with other data outside of background map.
 
-var map = L.map('map').setView([42.3, -83.15], 10);
+var map = L.map('map').setView([42.46, -83.18], 9);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -12,5 +12,11 @@ var existing = new L.GeoJSON.AJAX("data/Locations.geojson", {
 	onEachFeature: function(feature, layer) {
 		layer.bindPopup(feature.properties.City + "</br>" + feature.properties.Address + ", " +
 		feature.properties.state + " " + feature.properties.ZIP);
+	}
+}).addTo(map);
+
+var areas = new L.GeoJSON.AJAX("data/areas.geojson", {
+	onEachFeature: function(feature, layer) {
+		layer.bindPopup("Tract: " + feature.properties.GEOID + "</br> Population: " + feature.properties.pop_TOTAL);
 	}
 }).addTo(map);
